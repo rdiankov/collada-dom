038696d193e7f35e5c097c9d0f8d9245183fc95f
rdiankov <rosen.diankov@gmail.com>
2013-05-25 13:56:22 +0900

    minor changes to get deb packages working again

---
+++ release/copydebfiles.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""copies necessary files for deb source package building to a destination directory. The destination directory must not exist
+"""
+import os, sys
+import shutil
+
+if __name__=='__main__':
+    srcdir = sys.argv[1]
+    destdir = sys.argv[2]
+    
+    def ignorefiles(src, names):
+        if src == srcdir:
+            return ['.git', 'release']
+        
+        if src == os.path.join(srcdir,'dom'):
+            return ['test']
+        
+        if src == os.path.join(srcdir,'dom','external-libs'):
+            return ['libxml2-new', 'minizip-1.1', 'pcre-8.02', 'tinyxml', 'zlib-1.2.5']
+        
+        return []
+    
+    shutil.copytree(srcdir, destdir, ignore = ignorefiles)
