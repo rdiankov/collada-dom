a75557b2e997887ccd22b36f4cbfd5020a33dd8c
Pino Toscano <toscano.pino@tiscali.it>
2018-11-11 19:50:45 +0100

    daeUtils: handle more cases on GNU libc systems
    Fix cdom::getSystemTmpDir(), and cdom::getRandomFileName() on all the
    non-Linux systems that use GNU libc (e.g. kFreeBSD, and Hurd): use the
    same code of Linux paths.

--- dom/src/dae/daeUtils.cpp
+++ dom/src/dae/daeUtils.cpp
@@ -152,7 +152,7 @@ char cdom::getFileSeparator() {
 const string& cdom::getSystemTmpDir() {
 #ifdef WIN32
     static string tmpDir = string(getenv("TMP")) + getFileSeparator();
-#elif defined(__linux__) || defined(__linux)
+#elif defined(__linux__) || defined(__linux) || defined(__GLIBC__)
     static string tmpDir = "/tmp/";
 #elif defined __APPLE_CC__
 static string tmpDir = string(getenv("TMPDIR"));
@@ -171,7 +171,7 @@ string cdom::getRandomFileName() {
     std::string tmp(tmpnam(&tmpbuffer[0]));
 #ifdef WIN32
     randomSegment = tmp.substr(tmp.find_last_of('\\')+1);
-#elif defined(__linux__) || defined(__linux)
+#elif defined(__linux__) || defined(__linux) || defined(__GLIBC__)
     randomSegment = tmp.substr(tmp.find_last_of('/')+1);
 #elif defined __APPLE_CC__
 	randomSegment = tmp.substr(tmp.find_last_of('/')+1);
